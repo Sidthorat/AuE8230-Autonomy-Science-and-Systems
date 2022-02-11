c2985d85504b3cd001a2d6d5bba918d3a4be8065
Sidthorat <93834678+Sidthorat@users.noreply.github.com>
2022-02-10 23:21:12 -0500

    Delete catkin_ws directory

--- catkin_ws/build/assignment2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xander/AuE8230Spring22_Siddharththorat-/catkin_ws/src/assignment2
-# Build directory: /home/xander/AuE8230Spring22_Siddharththorat-/catkin_ws/build/assignment2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
